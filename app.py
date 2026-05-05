20b038597992499374df2369a7279907cbade36c
Matthew Scott <matthewscott364@gmail.com>
2026-05-05 05:42:22 -0400

    update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-def load_data():
-    df = pd.read_csv('C:/Users/matth/OneDrive/Desktop/DA/Personal_Projects/Business/cloudservicesfin-eda/CloudServiceFinancials.csv')
-    st.write(df)
-
-load_data()
+df = pd.read_csv(r'C:\Users\matth\OneDrive\Desktop\DA\Personal_Projects\Business\cloudservicesfin-eda\CloudServiceFinancials.csv')
+st.line_chart(df['Revenue'])
+st.write("Data Preview", df.head())
