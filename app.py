26db944ae5ae5e41c88e855ebc44ffdfe85a3ab3
Matthew Scott <matthewscott364@gmail.com>
2026-05-05 05:48:21 -0400

    small change

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv(r'C:\Users\matth\OneDrive\Desktop\DA\Personal_Projects\Business\cloudservicesfin-eda\CloudServiceFinancials.csv')
-st.line_chart(df['Revenue'])
+st.line_chart(df)
 st.write("Data Preview", df.head())
